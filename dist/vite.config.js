a8152ef9f4265320295d7e727931d3a5b7df8580
Jeeyunnie <sheer.lovelybaby@gmail.com>
2025-03-07 01:19:14 -0500

    Restore vite.config.js to root

--- dist/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/Assignment-2/',  // GitHub Pages 경로
-  build: {
-    outDir: 'dist', // 빌드된 파일을 'dist' 폴더에 저장
-  },
-});
